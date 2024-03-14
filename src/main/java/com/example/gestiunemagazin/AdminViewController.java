12c4a7321400e65800cd259120786613be56e054
AlexandruIacobescu <alexandru.iacobescu01@e-uvt.ro>
2024-03-15 01:31:28 +0200

    commit 6

--- src/main/java/com/example/gestiunemagazin/AdminViewController.java
+++ src/main/java/com/example/gestiunemagazin/AdminViewController.java
@@ -1,16 +1,21 @@
 package com.example.gestiunemagazin;
 
 import com.example.gestiunemagazin.entity.Employee;
+import com.example.gestiunemagazin.utils.Message;
+import com.example.gestiunemagazin.utils.ViewHelper;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Priority;
+import javafx.scene.paint.Color;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.net.URL;
@@ -24,7 +29,13 @@ public class AdminViewController implements Initializable {
     private TableView<Employee> tableView;
 
     @FXML
-    private Button reloadButton, backButton, insertButton, generateButton;
+    private TextArea sqlScriptTextArea, sqlResultSetTextArea;
+
+    @FXML
+    private Label infoLabel;
+
+    @FXML
+    private Button reloadButton, backButton, insertButton, generateButton, clearFieldsButton, executeButton;
 
     @FXML
     private TextField firstnameTextField, lastnameTextField, departmentTextField, idTextField, emailTextField, phoneNumberTextField, salaryTextField;
@@ -63,6 +74,7 @@ public class AdminViewController implements Initializable {
 
     @FXML
     public void onInsertButtonClick() {
+        boolean show = true;
         try{
             Connection conn = DriverManager.getConnection("jdbc:sqlite:inventory.db");
             String sql = """
@@ -79,40 +91,73 @@ public class AdminViewController implements Initializable {
             stmt.setString(7, salaryTextField.getText());
             stmt.executeUpdate();
         } catch (SQLException ex) {
-            Alert alert = new Alert(Alert.AlertType.ERROR);
-            alert.setTitle("Exception Dialog");
-            alert.setHeaderText("Employee insertion operation failed.");
-            alert.setContentText("Database error. Please expand for additional details.");
-
-            StringWriter sw = new StringWriter();
-            PrintWriter pw = new PrintWriter(sw);
-            pw.write(ex.getMessage());
-            String exceptionText = sw.toString();
-
-            Label label = new Label("The exception stacktrace was:");
-
-            TextArea textArea = new TextArea(exceptionText);
-            textArea.setEditable(false);
-            textArea.setWrapText(true);
-
-            textArea.setMaxWidth(Double.MAX_VALUE);
-            textArea.setMaxHeight(Double.MAX_VALUE);
-            GridPane.setVgrow(textArea, Priority.ALWAYS);
-            GridPane.setHgrow(textArea, Priority.ALWAYS);
-
-            GridPane expContent = new GridPane();
-            expContent.setMaxWidth(Double.MAX_VALUE);
-            expContent.add(label, 0, 0);
-            expContent.add(textArea, 0, 1);
-
-            alert.getDialogPane().setExpandableContent(expContent);
-            alert.showAndWait();
+            showErrorMessageDialog(
+                    "Exception Dialog",
+                    "Employee insertion operation failed.",
+                    "Database error. Please expand for additional details.",
+                    ex.getMessage()
+            );
+            show = false;
+        } finally {
+            if(show){
+                populateTableViewWithEmployees();
+                new Message(infoLabel, 5000, "Insertion Successful.", Color.DARKGREEN).show();
+            }
         }
+    }
+
+    @FXML
+    public void onBackButtonClick(ActionEvent event) throws IOException {
+        ViewHelper.openView(getClass(), event, "login-view.fxml", "Employee Login");
     }
 
     @FXML
     public void onReloadButtonClick() {
         populateTableViewWithEmployees();
+    }
+
+    @FXML
+    public void onClearAllFieldsButtonClick(){
+        firstnameTextField.setText("");
+        lastnameTextField.setText("");
+        departmentTextField.setText("");
+        idTextField.setText("");
+        emailTextField.setText("");
+        phoneNumberTextField.setText("");
+        salaryTextField.setText("");
+    }
+
+    @FXML
+    public void onExecuteButtonClick() {
+        sqlResultSetTextArea.clear();
+        try {
+            Connection conn = DriverManager.getConnection("jdbc:sqlite:inventory.db");
+            String sql = sqlScriptTextArea.getText();
+
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            ResultSet set = stmt.executeQuery();
+
+            ResultSetMetaData metaData = set.getMetaData();
+            int columnsNumber = metaData.getColumnCount();
+
+            // Print column headers
+            for (int i = 1; i <= columnsNumber; i++) {
+                sqlResultSetTextArea.appendText(String.format("%-20s", metaData.getColumnName(i)));
+            }
+            sqlResultSetTextArea.appendText("\n");
+
+            // Print rows
+            while (set.next()) {
+                for (int i = 1; i <= columnsNumber; i++) {
+                    String columnValue = set.getString(i);
+                    sqlResultSetTextArea.appendText(String.format("%-20s", columnValue));
+                }
+                sqlResultSetTextArea.appendText("\n");
+            }
+
+        } catch (SQLException ex) {
+            sqlResultSetTextArea.setText(ex.getMessage());
+        }
     }
 
     @Override
@@ -156,5 +201,36 @@ public class AdminViewController implements Initializable {
         } catch (SQLException ex) {
             ex.printStackTrace();
         }
+    }
+
+    private void showErrorMessageDialog(String title, String headerText, String contentText, String exceptionMessage) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle(title);
+        alert.setHeaderText(headerText);
+        alert.setContentText(contentText);
+
+        StringWriter sw = new StringWriter();
+        PrintWriter pw = new PrintWriter(sw);
+        pw.write(exceptionMessage);
+        String exceptionText = sw.toString();
+
+        Label label = new Label("The exception stacktrace was:");
+
+        TextArea textArea = new TextArea(exceptionText);
+        textArea.setEditable(false);
+        textArea.setWrapText(true);
+
+        textArea.setMaxWidth(Double.MAX_VALUE);
+        textArea.setMaxHeight(Double.MAX_VALUE);
+        GridPane.setVgrow(textArea, Priority.ALWAYS);
+        GridPane.setHgrow(textArea, Priority.ALWAYS);
+
+        GridPane expContent = new GridPane();
+        expContent.setMaxWidth(Double.MAX_VALUE);
+        expContent.add(label, 0, 0);
+        expContent.add(textArea, 0, 1);
+
+        alert.getDialogPane().setExpandableContent(expContent);
+        alert.showAndWait();
     }
 }
